3c0138464363f2693763ee98ffd48ed27c998d99
fakhirazahrany <katherinasalvator88@gmail.com>
2023-11-22 16:46:32 +0700

    enter to github

--- CodinganUtama.java
+++ CodinganUtama.java
@@ -71,6 +71,7 @@ public class CodinganUtama {
         }
 
         System.out.println("\nThank you for using the Expedition Java Application!");
+        System.out.println("\nThank you for using the Expedition Java Application!");
 
     }
 }
